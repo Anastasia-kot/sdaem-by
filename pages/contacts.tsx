05a71f0122bd8a40d2f4c9c212d05b60791e5052
Anastasia Kotelnikova <xtlbyhfrjnxfuty@gmail.com>
2023-02-03 19:22:26 +0300

    contacts refactoring

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -2,7 +2,5 @@ import { Contacts } from '../src/screens/Contacts/Contacts'
 import { Layout } from '../src/layout/Layout';
 import { FC } from 'react';
 
-
-
 const Home: FC = () =>  <Layout> <Contacts/> </Layout>
 export default Home
