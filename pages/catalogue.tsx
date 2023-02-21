e8f13bf991150f1c6eb706baf36da49b4c676777
Anastasia Kotelnikova <xtlbyhfrjnxfuty@gmail.com>
2023-02-21 12:23:09 +0300

    fix

--- pages/catalogue.tsx
+++ pages/catalogue.tsx
@@ -1,8 +1,8 @@
-import { FC } from 'react';
-import { Catalogue } from '../src/pages/Catalogue/ui/Catalogue'
-import { GetServerSideProps } from 'next';
-import { CatalogueState } from '../types/catalogue_data';
-import { Layout } from '../src/widgets/layout/Layout';
+  import { FC } from 'react';
+  import { Catalogue } from '../src/pages/Catalogue/ui/Catalogue'
+  import { GetServerSideProps } from 'next';
+  import { CatalogueState } from '../types/catalogue_data';
+  import { Layout } from '../src/widgets/layout/Layout';
 
 
 
@@ -10,24 +10,24 @@ import { Layout } from '../src/widgets/layout/Layout';
 
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+  export const getServerSideProps: GetServerSideProps = async () => {
 
-  try {
-    const res = await fetch(`http://localhost:3000/api/catalogue`)
-    const data = await res.json()
-    return { props: { data } }
+    try {
+      const res = await fetch(`http://localhost:3000/api/catalogue`)
+      const data = await res.json()
+      return { props: { data } }
 
-  } catch (err) {
-    return { props: { data: {items: null, totalCount: null} } }
+    } catch (err) {
+      return { props: { data: {items: null, totalCount: null} } }
+    }
+
   }
 
-}
+
+  type Props = {
+    data: CatalogueState
+  }
 
 
-type Props = {
-  data: CatalogueState
-}
-
-
-const Home: FC<Props> = ({ data }) => <Layout>  <Catalogue data={data}/>  </Layout>
-export default Home
+  const Home: FC<Props> = ({ data }) => <Layout>  <Catalogue data={data}/>  </Layout>
+  export default Home
--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,6 +1,6 @@
-import { Registration } from '../src/pages/Register/ui/Registration'
-import { FC } from 'react'
-  
-const Home: FC = () => <Registration />
-export default Home
+    import { Registration } from '../src/pages/Register/ui/Registration'
+    import { FC } from 'react'
+    
+    const Home: FC = () => <Registration />
+    export default Home
 
